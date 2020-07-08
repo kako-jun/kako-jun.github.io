ca727eac1f754d57a8e17a2652d845a3a127fba0
kako-jun <3541096+kako-jun@users.noreply.github.com>
2020-07-08 14:01:37 +0900

    update.

--- assets/js/my.js
+++ assets/js/my.js
@@ -1,139 +1,69 @@
-const loadCounter = () => {
-  // sample1
-  window.NostalgicCounter.beNostalgic(
-    "nostalgic-counter-sample1",
-    "https://nostalgic-counter.llll-ll.com/api/counter"
+window.onload = async () => {
+  const scroll = new SmoothScroll('a[href*="#"]', {
+    speed: 210,
+    speedAsDuration: true,
+    easing: "easeOutQuad",
+  });
+
+  const counter = await window.NostalgicCounter.getCounter(
+    "https://nostalgic-counter.llll-ll.com/api/counter?id=llll-ll&ex"
+    // "http://localhost:42011/api/counter"
   );
 
-  // sample2
-  window.NostalgicCounter.beNostalgic(
-    "nostalgic-counter-sample2",
-    "https://nostalgic-counter.llll-ll.com/api/counter",
-    {
-      zero_padding_length: 8
-    }
-  );
-
-  // sample3
-  window.NostalgicCounter.beNostalgic(
-    "nostalgic-counter-sample3",
-    "https://nostalgic-counter.llll-ll.com/api/counter?id=sample1"
-  );
-
-  // sample4
-  window.NostalgicCounter.beNostalgic(
-    "nostalgic-counter-sample4",
-    "https://nostalgic-counter.llll-ll.com/api/counter?id=sample1",
-    { zero_padding_length: 8 }
-  );
-
-  // sample5
-  window.NostalgicCounter.beNostalgic(
-    "nostalgic-counter-sample5",
-    "https://nostalgic-counter.llll-ll.com/api/counter?id=sample2",
-    { image_dir_path: "./assets/image/counter/pdy", image_ext: ".gif" }
-  );
-
-  // sample6
-  window.NostalgicCounter.beNostalgic(
-    "nostalgic-counter-sample6",
-    "https://nostalgic-counter.llll-ll.com/api/counter?id=sample2",
-    {
-      zero_padding_length: 8,
+  if (counter) {
+    window.NostalgicCounter.showCounter("nostalgic-counter", counter.total, {
       image_dir_path: "./assets/image/counter/pdy",
-      image_ext: ".gif"
-    }
-  );
+      image_ext: ".gif",
+    });
 
-  // sample7
-  window.NostalgicCounter.beNostalgic(
-    "nostalgic-counter-sample7",
-    "https://nostalgic-counter.llll-ll.com/api/counter?id=sample2",
-    {
-      zero_padding_length: 8,
-      image_dir_path: "./assets/image/counter/led",
-      image_ext: ".gif"
-    }
-  );
-
-  // sample8
-  window.NostalgicCounter.beNostalgic(
-    "nostalgic-counter-sample8",
-    "https://nostalgic-counter.llll-ll.com/api/counter?id=sample2",
-    {
-      zero_padding_length: 8,
-      image_dir_path: "./assets/image/counter/mahjong",
-      image_ext: ".png"
-    }
-  );
-
-  // sample9
-  window.NostalgicCounter.beNostalgic(
-    "nostalgic-counter-sample9",
-    "https://nostalgic-counter.llll-ll.com/api/counter?id=sample3",
-    {
+    window.NostalgicCounter.showKiriban("nostalgic-counter-kiriban", counter.total, {
       normal_messages: [
         {
           step: 10,
           message:
-            'キリが良いよ！ <a href="https://twitter.com/share?text={total} 番目の訪問者でした！">シェアする</a>'
+            '<p>{step} で割り切れるので、キリが良いよ！ <a class="button button-blue" href="https://twitter.com/share?text={raw_count} 番目の訪問者でした！" target="_blank"><i class="fab fa-twitter"></i> シェアする</a></p>',
         },
-        { step: 100, message: "すごくキリが良いよ！" },
-        { step: 1000, message: "めちゃくちゃキリが良いよ！" },
-        { step: 10000, message: "キリ良すぎ！" }
+        { step: 100, message: "<p>{step} で割り切れるので、すごくキリが良いよ！</p>" },
+        { step: 1000, message: "<p>{step} で割り切れるので、めちゃくちゃキリが良いよ！</p>" },
+        { step: 10000, message: "<p>{step} で割り切れるので、キリ良すぎ！</p>" },
       ],
       special_messages: [
-        { total: 10, message: "みんなありがとう" },
-        { total: 20, message: "フン" },
-        { total: 30, message: "神に感謝" },
-        { total: 40, message: "くっ ボーボボに負けた…" },
-        { total: 50, message: "順当な順位ですね" },
+        { count: 10, message: "<p>みんなありがとう</p>" },
+        { count: 20, message: "<p>フン</p>" },
+        { count: 30, message: "<p>神に感謝</p>" },
+        { count: 40, message: "<p>くっ ボーボボに負けた…</p>" },
+        { count: 50, message: "<p>順当な順位ですね</p>" },
         {
-          total: 60,
-          message: "それではどうぞ。キユで「ロケットでつきぬけろ！」"
+          count: 60,
+          message: "<p>それではどうぞ。キユで「ロケットでつきぬけろ！」</p>",
         },
         {
-          total: 70,
-          message:
-            "ここで次週予告！！来週はハルタのお母さん登場！！マザー・オブ・ラブでつきぬけろ！"
+          count: 70,
+          message: "<p>ここで次週予告！！来週はハルタのお母さん登場！！マザー・オブ・ラブでつきぬけろ！</p>",
         },
         {
-          total: 80,
-          message:
-            "次週予告！！いよいよ赤城がベールを脱ぐ！！赤城の目的は！？ヒステリックにつきぬけろ！"
+          count: 80,
+          message: "<p>次週予告！！いよいよ赤城がベールを脱ぐ！！赤城の目的は！？ヒステリックにつきぬけろ！</p>",
         },
         {
-          total: 90,
-          message:
-            "毎回この欄はボツを食う。けどそれは自分が大人でありコドモであるとゆう事の誇りだ"
+          count: 90,
+          message: "<p>毎回この欄はボツを食う。けどそれは自分が大人でありコドモであるとゆう事の誇りだ</p>",
         },
         {
-          total: 100,
-          message:
-            "痛みを知らない子供が嫌い。心をなくした大人が嫌い。優しい漫画が好き。バイバイ"
-        }
+          count: 100,
+          message: "<p>痛みを知らない子供が嫌い。心をなくした大人が嫌い。優しい漫画が好き。バイバイ</p>",
+        },
+        { count: 42038, message: "<p>{count} 回も来てくれて感謝。次は {next} を狙って。</p>" },
       ],
-      no_kiriban_message: "<p>残念！ キリ番ではありません。</p>",
-      no_more_kiriban_message: "<p>もうキリ番はありません</p>",
-      next_kiriban_message_left: "<p>次のキリ番は ",
-      next_kiriban_message_right: " です</p>"
+      no_kiriban_message: "",
+      no_more_kiriban_message: "<p>もうキリ番はありません。</p>",
+      next_kiriban_message: "<p>次のキリ番は {next} です。</p>",
+    });
+
+    const targetElement = document.getElementById("nostalgic-counter-past");
+    if (targetElement) {
+      let html = "<ul><li>昨日 " + counter.yesterday_date + " は " + counter.yesterday + " 人でした。</li>";
+      targetElement.innerHTML = html;
     }
-  );
-};
-
-window.onload = () => {
-  const scroll = new SmoothScroll('a[href*="#"]', {
-    speed: 210,
-    speedAsDuration: true,
-    easing: "easeOutQuad"
-  });
-
-  window.NostalgicCounter.showCounter(
-    "nostalgic-counter",
-    "https://nostalgic-counter.llll-ll.com/api/counter",
-    // {
-    //   image_dir_path: "./assets/image/counter/pdy",
-    //   image_ext: ".gif"
-    // }
-  );
+  }
 };
